96385a8e5fc32eb5ed65939725d9e44e12d572ef
markert <florian@scienceflow.de>
2013-11-07 14:05:07 +0100

    added root+leaf search

--- radix.lua
+++ radix.lua
@@ -5,6 +5,7 @@ new = function(config)
   local j = {}
   local radix_tree = {}
   local radix_elements = {}
+  local temp_instance = nil
   
   local lookup_fsm = function (wordpart, next_state, next_letter)
     if (wordpart:sub(next_state,next_state) ~= next_letter) then
@@ -18,36 +19,36 @@ new = function(config)
   end
   
   local add_to_tree
-  add_to_tree = function( a, fullword, part )
+  add_to_tree = function( tree_instance, fullword, part )
     part = part or fullword;
     if part:len() < 1 then
-      a[fullword]=true;
+      tree_instance[fullword]=true;
     else
       local s = part:sub( 1, 1 )
-      if type(a[s])~="table" then
-        a[s] = {};
+      if type(tree_instance[s])~="table" then
+        tree_instance[s] = {};
       end
-      add_to_tree( a[s], fullword, part:sub(2) )
+      add_to_tree( tree_instance[s], fullword, part:sub(2) )
     end
   end
   
   local remove_from_tree
-  remove_from_tree = function( a, fullword, part )
+  remove_from_tree = function( tree_instance, fullword, part )
     part = part or fullword;
     if part:len() < 1 then
-      a[fullword]=nil;
+      tree_instance[fullword]=nil;
     else
       local s = part:sub( 1, 1 )
-      if type(a[s])~="table" then
-        a[s] = {};
+      if type(tree_instance[s])~="table" then
+        tree_instance[s] = {};
       end
-      remove_from_tree( a[s], fullword, part:sub(2) )
+      remove_from_tree( tree_instance[s], fullword, part:sub(2) )
     end
   end
   
   local radix_traverse
-  radix_traverse = function( a )
-    for k, v in pairs(a) do
+  radix_traverse = function( tree_instance )
+    for k, v in pairs(tree_instance) do
       if type(v)=="boolean" then
         radix_elements[k] = true
       elseif type(v)=="table" then
@@ -57,21 +58,24 @@ new = function(config)
   end
   
   local root_lookup
-  root_lookup = function( a, part )
+  root_lookup = function( tree_instance, part, traverse)
     if part:len() < 1 then
-      radix_traverse( a )
+      temp_instance = tree_instance
+      if (traverse) then
+        radix_traverse( tree_instance )
+      end
     else
       local s = part:sub( 1, 1 )
-      if type(a[s])=="table" then
-        root_lookup( a[s], part:sub(2) )
+      if type(tree_instance[s])=="table" then
+        root_lookup( tree_instance[s], part:sub(2), traverse)
       end
     end
   end
   
   local leaf_lookup
-  leaf_lookup = function( a, word, state, only_end )
+  leaf_lookup = function( tree_instance, word, state, only_end )
     local next_state = state+1
-    for k, v in pairs(a) do
+    for k, v in pairs(tree_instance) do
       if type(v)=="table" then
         local hit, next_state = lookup_fsm(word, next_state, k)
         if (hit == true) then
@@ -89,6 +93,12 @@ new = function(config)
     end
   end
   
+  local root_leaf_lookup
+  root_leaf_lookup = function(tree_instance, wordstart, wordend)
+    root_lookup(tree_instance, wordstart)
+    leaf_lookup(temp_instance, wordend, 0, true)
+  end
+  
   j.add = add_to_tree
   j.add_main = function (word)
     add_to_tree(radix_tree, word)
@@ -103,11 +113,18 @@ new = function(config)
   end
   j.root_lookup = root_lookup
   j.root_lookup_main = function (word)
-    root_lookup(radix_tree, word)
+    root_lookup(radix_tree, word, true)
+  end
+  j.root_leaf_lookup = root_leaf_lookup
+  j.root_leaf_lookup_main = function (wordstart, wordend)
+    root_leaf_lookup(radix_tree, wordstart, wordend)
   end
   j.leaf_lookup = leaf_lookup
-  j.leaf_lookup_main = function (word, only_leafs)
-    leaf_lookup(radix_tree, word, 0, only_leafs)
+  j.leaf_lookup_main = function (word)
+    leaf_lookup(radix_tree, word, 0, true)
+  end
+  j.anypos_lookup_main = function (word)
+    leaf_lookup(radix_tree, word, 0, false)
   end
   j.reset_elements = function ()
     for k,v in pairs(radix_elements) do radix_elements[k]=nil end
